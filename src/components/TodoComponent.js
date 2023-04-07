a7a2c9ad3e39d0b3eb03a4a08e6b85d4115f4198
dongyeopCha <ckehdduq95@gmail.com>
2023-04-07 16:59:50 +0900

    "feat:#assignment8" todo 우측에 수정버튼과 삭제 버튼 생성

--- src/components/TodoComponent.js
+++ src/components/TodoComponent.js
@@ -15,6 +15,8 @@ function TodoComponent(props) {
         <input type="checkbox" checked={isCompleted} onChange={isCompletedChangeHandler} />
         <span>{todo}</span>
       </label>
+      <button data-testid="modify-button">수정</button>
+      <button data-testid="delete-button">삭제</button>
     </li>
   );
 }
